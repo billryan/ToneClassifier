388d97c3c7e81e84c3b3cf6091fd75c59119ab1d
billryan <yuanbin2014@gmail.com>
2014-06-20 17:07:45 +0800

    add feature normalization function

---
+++ featureNorm.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+# feature normalization with average and variance
+def featureNorm1(datasetDic, intervalNum):
+    featureDic = {}
+    for key in datasetDic.keys():
+        f0List = datasetDic[key]['f0']
+        engyList = datasetDic[key]['engy']
+        i = 0
+        j = len(f0List)
+        step = j / intervalNum
+        spacedPointsList = [i for i in xrange(0, j - (j % intervalNum) + 1, step)] 
+        spacedPointsList[-1] = max(spacedPointsList[-1],j)
+        #curStepListLen = len(curStepList)
+        for i in xrange(intervalNum):
+            minJ = spacedPointsList[i]
+            maxJ = spacedPointsList[i + 1]
+            f0Mu = float(np.mean(f0List[minJ:maxJ]))
+            engyMu = float(np.mean(engyList[minJ:maxJ]))
+            f0Sigma = float(np.std(f0List[minJ:maxJ]))
+            engySigma = float(np.std(engyList[minJ:maxJ]))
+            if featureDic.has_key(key):
+                featureDic[key]['feature'].extend([f0Mu, f0Sigma, engyMu, engySigma])
+            else:
+                featureDic[key] = {}
+                featureDic[key]['feature'] = []
+                featureDic[key]['label'] = dataDic[key]['label']
+                featureDic[key]['feature'].extend([f0Mu, f0Sigma, engyMu, engySigma])
+    return featureDic
