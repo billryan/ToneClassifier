ee499fc5e05c4bfe9ec37cb056d1635a93f113b8
billryan <yuanbin2014@gmail.com>
2014-06-21 09:36:44 +0800

    add dataset for different moving average window size

--- preProcess.py
+++ preProcess.py
@@ -8,10 +8,6 @@ if (len(sys.argv) != 2):
     sys.exit(1);
 
 path = sys.argv[1]
-if (path[-1] == '/'):
-    dataset_new = path[:-1] + '_pos/'
-else:
-    dataset_new = path + '_pos/'
 
 #if not (os.path.exists(dataset_new)):
 #    os.makedirs(dataset_new)
@@ -51,30 +47,35 @@ def get_recursive_file_list(path):
 get_recursive_file_list(path)
 
 # moving average filter
-window = 5
-maf = []
-for i in xrange(window):
-    maf.append(1.0/window)
-
-# max interval non-zero
-inter_nonzero = []
-#calculate the average value of pitch
-for fn in f0files:
-    strs = fn.split(".")
-    engyfn = strs[0]+".engy"
-    feature_f0 = np.loadtxt(fn)
-    #feature_f0 = np.convolve(feature_f0, maf, 'same')
-    feature_f0 = np.convolve(feature_f0, maf, 'valid')
-    feature_engy = np.loadtxt(engyfn)
-    feature_engy = np.convolve(feature_engy, maf, 'same')
-
-    pos_index = get_max_interval_nonzero(feature_f0)
-    feature_f0_new = np.take(feature_f0, pos_index)
-    feature_engy_new = np.take(feature_engy, pos_index)
-
-    dirs = fn.split('/')
-    dataset_newdir = dataset_new+dirs[-3]+'/'+dirs[-2]+'/'
-    if not (os.path.exists(dataset_newdir)):
-        os.makedirs(dataset_newdir)
-    np.savetxt(dataset_newdir+os.path.basename(fn),feature_f0_new, delimiter='\n',fmt='%s')
-    np.savetxt(dataset_newdir+os.path.basename(fn).replace('f0','engy'),feature_engy_new, delimiter='\n',fmt='%s')
+for window in xrange(3, 11):
+    maf = []
+    for i in xrange(window):
+        maf.append(1.0/window)
+    
+    if (path[-1] == '/'):
+        dataset_new = path[:-1] + '-w' + str(window) + '/'
+    else:
+        dataset_new = path + '-w' + str(window) + '/'
+    
+    # max interval non-zero
+    inter_nonzero = []
+    #calculate the average value of pitch
+    for fn in f0files:
+        strs = fn.split(".")
+        engyfn = strs[0]+".engy"
+        feature_f0 = np.loadtxt(fn)
+        #feature_f0 = np.convolve(feature_f0, maf, 'same')
+        feature_f0 = np.convolve(feature_f0, maf, 'valid')
+        feature_engy = np.loadtxt(engyfn)
+        feature_engy = np.convolve(feature_engy, maf, 'same')
+    
+        pos_index = get_max_interval_nonzero(feature_f0)
+        feature_f0_new = np.take(feature_f0, pos_index)
+        feature_engy_new = np.take(feature_engy, pos_index)
+    
+        dirs = fn.split('/')
+        dataset_newdir = dataset_new+dirs[-3]+'/'+dirs[-2]+'/'
+        if not (os.path.exists(dataset_newdir)):
+            os.makedirs(dataset_newdir)
+        np.savetxt(dataset_newdir+os.path.basename(fn),feature_f0_new, delimiter='\n',fmt='%s')
+        np.savetxt(dataset_newdir+os.path.basename(fn).replace('f0','engy'),feature_engy_new, delimiter='\n',fmt='%s')
